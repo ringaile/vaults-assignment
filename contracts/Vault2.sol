2cd788de559c609db012a14ce2834a42ca82d6ac
Ringaile Valiaugaite <ringaile@gmail.com>
2022-01-23 09:36:18 +0000

    added vault2 impl

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,24 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault2 {
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault2 is ERC20{
+
+    constructor() ERC20("Wrapped Vault", "WVT"){}
+
+    function mint(uint256 _amount) external payable {
+        require(_amount >= 0, "No amount given");
+        require(_amount == msg.value, "Amount must be the same as ETH value");
+        _mint(msg.sender, _amount);
+    }
+    
+    function burn(uint256 _amount) external payable {
+        require(_amount > 0, "No amount given");
+        _burn(msg.sender, _amount);
+        (bool sent,) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send ETH");
+    }
     
 }
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -2,6 +2,17 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Vault 2", () => {
+	let Vault2;
+    let vault2;
+    let TestToken;
+	let testToken;
+	let owner;
+    let addr1;
+    let addr2;
+    let addr3;
+    let addrs;
+	const tokenSupply = 10000000000000;
+
 	beforeEach(async () => {
 		Vault2 = await ethers.getContractFactory("Vault2");
 		[owner] = await ethers.getSigners();
@@ -9,5 +20,14 @@ describe("Vault 2", () => {
 		vault2 = await Vault2.deploy();
 
 		await vault2.deployed();
+
+		// const approveTx = await testToken.connect(vault2).approve(vault2.address, tokenSupply);
+    	// await approveTx.wait();
+	});
+
+	describe("Deployment", function () {
+		it("Should set the right owner", async function () {
+		  expect(await vault2.owner()).to.equal(owner.address);
+		});
 	});
 });
