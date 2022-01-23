ab3c97d079e6bb649503e99ca0e4f093a6ca838b
Ringaile Valiaugaite <ringaile@gmail.com>
2022-01-23 10:20:33 +0000

    added vault2 tests

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -6,19 +6,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Vault2 is ERC20{
 
+    event Minted(address indexed user, uint256 amount);
+    event Burned(address indexed user, uint256 amount);
+
     constructor() ERC20("Wrapped Vault", "WVT"){}
 
     function mint(uint256 _amount) external payable {
-        require(_amount >= 0, "No amount given");
+        require(_amount > 0, "No amount given");
         require(_amount == msg.value, "Amount must be the same as ETH value");
         _mint(msg.sender, _amount);
+        emit Minted(msg.sender, _amount);
     }
     
     function burn(uint256 _amount) external payable {
+        console.log(_amount > 0);
         require(_amount > 0, "No amount given");
+        console.log(_amount);
         _burn(msg.sender, _amount);
         (bool sent,) = msg.sender.call{value: _amount}("");
         require(sent, "Failed to send ETH");
+        emit Burned(msg.sender, _amount);
     }
     
 }
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -4,30 +4,105 @@ const { ethers } = require("hardhat");
 describe("Vault 2", () => {
 	let Vault2;
     let vault2;
-    let TestToken;
-	let testToken;
 	let owner;
     let addr1;
     let addr2;
     let addr3;
     let addrs;
-	const tokenSupply = 10000000000000;
 
 	beforeEach(async () => {
 		Vault2 = await ethers.getContractFactory("Vault2");
-		[owner] = await ethers.getSigners();
+		[owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
 
 		vault2 = await Vault2.deploy();
-
 		await vault2.deployed();
-
-		// const approveTx = await testToken.connect(vault2).approve(vault2.address, tokenSupply);
-    	// await approveTx.wait();
 	});
 
-	describe("Deployment", function () {
-		it("Should set the right owner", async function () {
-		  expect(await vault2.owner()).to.equal(owner.address);
+	describe("Mint", function () {
+		it("Should allow to mint tokens", async function () {
+			let amount = 1;
+
+			const mintTx = await vault2.connect(owner).mint(amount, { value: amount });
+			await mintTx.wait();
+
+			await expect(mintTx).to.emit(vault2, 'Minted').withArgs(owner.address, amount);
+	  
+			expect(await vault2.totalSupply()).to.equal(amount);
+		});
+
+		it("Should allow to mint tokens from any address", async function () {
+			let amount = 1;
+
+			const mintTx = await vault2.connect(addr1).mint(amount, { value: amount });
+			await mintTx.wait();
+
+			await expect(mintTx).to.emit(vault2, 'Minted').withArgs(addr1.address, amount);
+	  
+			expect(await vault2.totalSupply()).to.equal(amount);
 		});
 	});
+
+	describe("Mint Errors", function () {
+		it("Should not allow to mint tokens if amount is different from eth sent", async function () {
+			let amount = 1;
+
+			expect(vault2.connect(owner).mint(amount + 1, { value: amount })).to.be.revertedWith("Amount must be the same as ETH value");
+	  
+			expect(await vault2.totalSupply()).to.equal(0);
+		});
+
+		it("Should not allow to mint tokens if amount is zero or less", async function () {
+			let amount = 0;
+
+			expect(vault2.connect(owner).mint(amount, { value: amount })).to.be.revertedWith("No amount given");
+	  
+			expect(await vault2.totalSupply()).to.equal(0);
+		});
+	});
+
+	describe("Burn errors", function () {
+		it("Should not allow to burn tokens if amount is zero or less", async function () {
+			let amount = 0;
+
+			expect(vault2.connect(owner).burn(amount)).to.be.revertedWith("No amount given");
+	  
+			expect(await vault2.totalSupply()).to.equal(0);
+		});
+	});	
+
+	describe("Burn", function () {
+		it("Should allow to burn tokens", async function () {
+			let mintAmount = 5;
+			let burnAmount = 1
+
+			const mintTx = await vault2.connect(owner).mint(mintAmount, { value: mintAmount });
+			await mintTx.wait();
+
+			await expect(mintTx).to.emit(vault2, 'Minted').withArgs(owner.address, mintAmount);
+	  
+			expect(await vault2.totalSupply()).to.equal(mintAmount);
+
+			const burnTx = await vault2.connect(owner).burn(burnAmount);
+			await burnTx.wait();
+			
+			expect(await vault2.totalSupply()).to.equal(mintAmount - burnAmount);
+		});
+		
+		it("Should allow to burn tokens from any address", async function () {
+			let mintAmount = 5;
+			let burnAmount = 1
+
+			const mintTx = await vault2.connect(addr1).mint(mintAmount, { value: mintAmount });
+			await mintTx.wait();
+
+			await expect(mintTx).to.emit(vault2, 'Minted').withArgs(addr1.address, mintAmount);
+	  
+			expect(await vault2.totalSupply()).to.equal(mintAmount);
+
+			const burnTx = await vault2.connect(addr1).burn(burnAmount);
+			await burnTx.wait();
+			
+			expect(await vault2.totalSupply()).to.equal(mintAmount - burnAmount);
+		});
+	});	
 });
